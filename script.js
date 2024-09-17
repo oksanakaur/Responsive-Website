29d8db68d799a8415a6a84c9c6c2c42c5df0481c
oksanakaur <160805189+oksanakaur@users.noreply.github.com>
2024-09-17 11:20:57 +0200

    Added css and Javascript

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener ("Scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 150);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlinks = document.querySelector('.navlinks');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('open');
+}
+
+windows.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlinks.classList.remove('open');
+}
